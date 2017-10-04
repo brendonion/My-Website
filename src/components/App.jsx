1752303f9b28fab4226478db07a5a095dd713ceb
Brendan Walker <brenny_walker@hotmail.com>
2017-10-03 19:14:37 -0700

    Adds the rest of the styling minus a few pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,10 +10,122 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="main">
-          <div className="section half">
-            <div className="container">
+          <div className="section">
+            <div className="profile container">
               <img className="profile-pic" src="/assets/LighthouseProfile.jpg" />
+              <div className="meta-container">
+                <h1>Brendan Walker</h1>
+                <h3>Web Developer</h3>
+                <p>I’m a web developer who has a passion for technology and design. I was inspired to work for the tech industry while touring my cousin’s tech company. I knew right away that this was something I wanted to pursue. Thus, I’m ever so eager to code and discover the possibilities of the web.</p>
+              </div>
             </div>
+            <div className="info container">
+              <p><img src="/assets/Phone.png" />250-857-2365</p>
+              <p className="link" ><img src="/assets/Email.png" />brenny.walker@gmail.com</p>
+              <p>
+                <img className="github" src="/assets/GitHub.png" />
+                <a href="https://github.com/brendonion" target="_blank">brendonion</a>
+              </p>
+            </div>
+          </div>
+          <div className="section blue">
+            <h1 className="header">EXPERIENCE</h1>
+            <div className="experience container">
+              PLACEHOLDER
+            </div>
+          </div>
+          <div className="section">
+            <h1 className="header">EDUCATION</h1>
+            <div className="education container">
+              PLACEHOLDER
+            </div>
+          </div>
+          <div className="section blue">
+            <h1 className="header">SKILLS</h1>
+            <div className="skills">
+              <div className="skill container">
+                <img src="/assets/Javascript.png" />
+                <p>Javascript</p>
+              </div>
+              <div className="skill container">
+                <img src="/assets/HTML.png" />
+                <p>HTML</p>
+              </div>
+              <div className="skill container">
+                <img src="/assets/CSS.png" />
+                <p>CSS</p>
+              </div>
+              <div className="skill container">
+                <img src="/assets/Sass.png" />
+                <p>SASS</p>
+              </div>
+              <div className="skill container">
+                <img src="/assets/React.png" />
+                <p>React</p>
+              </div>
+              <div className="skill container">
+                <img src="/assets/Redux.png" />
+                <p>Redux</p>
+              </div>
+              <div className="skill container">
+                <img src="" />
+                <p>Golang</p>
+              </div>
+              <div className="skill container">
+                <img src="/assets/Ruby.png" />
+                <p>Ruby</p>
+              </div>
+              <div className="skill container">
+                <img src="" />
+                <p>Postgres</p>
+              </div>
+              <div className="skill container">
+                <img src="" />
+                <p>Bootstrap</p>
+              </div>
+              <div className="skill container">
+                <img src="/assets/Rails.png" />
+                <p>Rails</p>
+              </div>
+              <div className="skill container">
+                <img src="" />
+                <p>Express</p>
+              </div>
+              <div className="skill container">
+                <img src="" />
+                <p>MongoDB</p>
+              </div>
+              <div className="skill container">
+                <img src="" />
+                <p>Firebase</p>
+              </div>
+              <div className="skill container">
+                <img src="/assets/jQuery.png" />
+                <p>jQuery</p>
+              </div>
+              <div className="skill container">
+                <img src="" />
+                <p>Node</p>
+              </div>
+            </div>
+          </div>
+          <div className="section">
+            <h1 className="header">PROJECTS</h1>
+            <div className="container">
+              PLACEHOLDER
+            </div>
+          </div>
+          <div className="footer blue">
+            <h5 className="header">Brendan Walker</h5>
+            <div className="info container">
+              <p><img src="/assets/Phone.png" />250-857-2365</p>
+              <p className="link" ><img src="/assets/Email.png" />brenny.walker@gmail.com</p>
+              <p>
+                <img className="github" src="/assets/GitHub.png" />
+                <a href="https://github.com/brendonion" target="_blank">brendonion</a>
+              </p>
+            </div>
+            <button className="up-btn" />
           </div>
         </div>
       </div>
