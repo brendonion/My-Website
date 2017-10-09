27f7c63c66b84aef55e604fb4d9aee418f652731
Brendan Walker <brenny_walker@hotmail.com>
2017-10-08 22:04:02 -0700

    fixed footer email link

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,10 @@ const About = (props) => {
       </div>
       <div className="info container">
         <p><img src="/assets/Phone.png" />250-857-2365</p>
-        <p><img src="/assets/Email.png" /><a href={"mailto:brenny.walker@gmail.com"}>brenny.walker@gmail.com</a></p>
+        <p>
+          <img src="/assets/Email.png" />
+          <a href={"mailto:brenny.walker@gmail.com"}>brenny.walker@gmail.com</a>
+        </p>
         <p>
           <img className="github" src="/assets/GitHub.png" />
           <a href="https://github.com/brendonion" target="_blank">brendonion</a>
