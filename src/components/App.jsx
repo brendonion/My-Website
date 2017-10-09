2d5f1b639296c0812bce1387e4b38539619d2a4f
Brendan Walker <brenny_walker@hotmail.com>
2017-10-09 12:50:47 -0700

    Cleans up code base

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import { CSSTransitionGroup } from 'react-transition-group';
 
 import Navbar from './Navbar.jsx';
@@ -10,31 +9,28 @@ import Skills from './Skills.jsx';
 import Projects from './Projects.jsx';
 import Footer from './Footer.jsx';
 
-class App extends Component {
-  
-  render() {
-    return (
-      <div className="App">
-        <Navbar />
-        <div className="main">
-          <CSSTransitionGroup
-            transitionName="mounted"
-            transitionAppear={true}
-            transitionEnter={false}
-            transitionLeave={false}
-            transitionAppearTimeout={2000}
-          >
-            <About />
-          </CSSTransitionGroup>
-          <Experience />
-          <Education />
-          <Skills />
-          <Projects />
-          <Footer />
-        </div>
+const App = (props) => {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="main">
+        <CSSTransitionGroup
+          transitionName="mounted"
+          transitionAppear={true}
+          transitionEnter={false}
+          transitionLeave={false}
+          transitionAppearTimeout={2000}
+        >
+          <About />
+        </CSSTransitionGroup>
+        <Experience />
+        <Education />
+        <Skills />
+        <Projects />
+        <Footer />
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default App
